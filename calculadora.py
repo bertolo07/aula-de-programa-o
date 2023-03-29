3613c421297ccb82ab83a1be7ff21b6acc2de41e
bertolo07 <128647716+bertolo07@users.noreply.github.com>
2023-03-29 17:16:28 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+a=float(input())         #float = números decimais 
+c=input()
+b=float(input())           #int = números inteiros
+
+if c=="+":                 
+    print(a+b)              #depois de uma condição, como if no final ":"
+elif c=="-": 
+    print(a-b)
+elif c=="*": 
+    print(a*b)
+elif c=="/": 
+    print(a/b)
