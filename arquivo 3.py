6e636962be57fdfb5c77de377d6fb5827aa30485
bertolo07 <128647716+bertolo07@users.noreply.github.com>
2023-05-31 18:51:27 -0300

    Add files via upload

---
+++ arquivo 3.py
@@ -0,0 +1,33 @@
+#Objetivo: Criar um programa que organize uma lista de nomes em ordem alfabetica
+#Bonus: Organizar por ordem alfabetica da ultima letra do nome
+
+
+
+lista=["Alexandre", "Alice", "André", "Arthur", "Arthur", "Artur", "Augusto", "Bernardo", "Bernardo", "Bruno", "Davi","Diego","Eduardo","Fabrício","Felipe","Fernando","Francisco","Francisco","Gabriel","Gabriel","Giovanna","Giovanni","Guilherme","Guilherme","Hector","Henrique","Inácio","João","João","Joaquim","Júlia","Lauren","Leonardo","Leonardo","Lucas","Marina","Matheus","Matheus","Paula","Pedro","Pedro","Pedro","Pedro","Rafael","Regis","Sofia","Stella","Thiago","Valentina","Vicente","Lucas"]
+
+lista_ordenados=sorted(lista)
+
+print(lista_ordenados)
+
+
+#for i in range(len(lista)):
+    #for a in range(len(lista)-1):
+        #if lista[a] > lista[a+1]:
+            #lista[a], lista[a+1] = lista[a+1], lista[a]
+            #print[lista]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
